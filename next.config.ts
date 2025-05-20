7f4e9c9ca2cd185e3d636e56233661939f5809c8
notsawadawa177 <sawonok177@gmail.com>
2025-05-20 22:34:33 +0500

    Initial commit: Minecraft Social Network

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: ['mc-heads.net'], // Разрешаем загрузку изображений с mc-heads.net
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000'],
+    },
+  },
 };
 
 export default nextConfig;
